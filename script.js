3c61c5c110b07643da029090d8435a72aa371665
IFAlice <mr.spankibaster@gmail.com>
2021-12-10 22:59:24 +0200

    fixed 'if' in event func

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 const valueTextInput = document.querySelector('.main-container__input-text')
 
 valueTextInput.oninput = function (){
+    console.log(document.querySelector('.main-container__input-text').value);
+    // document.querySelector('.main-container__input-button').setAttribute('disabled', 'disabled')
+    
     document.querySelector('.flex-space__box-dinamic').style.cssText = `${document.querySelector(".main-container__input-text").value}`
-    const coordElem = document.querySelector('.flex-space__item-dinamic')
-    if(coordElem.offsetLeft == 450 && coordElem.offsetTop == 5){
-        document.querySelector('.main-container__input-button').removeAttribute('disabled')
-        
-    }     
+    
+    
+    const inputText = document.querySelector('.main-container__input-text')
+    if(inputText.value.slice(-1) !=';'){
+        if(inputText.value.split(' ').join('')+';'=='justify-content:center;'){
+            document.querySelector('.main-container__input-button').removeAttribute('disabled');      
+        }else if(inputText.value.slice(-1) ==';'){
+            if(inputText.value.split(' ').join('')=='justify-content:center;'){
+                document.querySelector('.main-container__input-button').removeAttribute('disabled');   
+            }
+        }
+        else{
+            document.querySelector('.main-container__input-button').setAttribute('disabled', 'disabled')
+        }
+    }
+    
 }
 
-function clickButton(){
-    const getMainContainer = document.querySelector('.main-container').style.display = 'none'
-    
-    document.querySelector('#congratulations-block').classList.add('congratulations-block__style') 
-    
-    document.querySelector('#congratulations-block__text').textContent = 'CONGRATULATIONS' 
-}
-const coordElem = document.querySelector('.flex-space__item-dinamic').offsetLeft
 
-console.log(coordElem);
+function lvlTwo(){
+    getClassName = document.querySelector('.flex-space__box-static')
+    replaceObjectsClass(getClassName,'box-static_flex-lvl-one-jc-center','box-static_flex-lvl-two-jc-flex-end')
+    document.querySelector('.main-container__input-text').value = '';
+    document.querySelector('.flex-space__box-dinamic').style.cssText = `justify-content:flex-start;`;
+}
+
+
+// document.querySelector('.flex-space__box-dinamic').style.cssText = `${document.querySelector(".main-container__input-text").value}`
+
+
+function replaceObjectsClass(object,firstNameObjectClass, secondNameObjectClass){
+    object.classList.remove(firstNameObjectClass);
+    object.classList.add(secondNameObjectClass);
+}
+
+
+// function clickButton(){
+//     const getMainContainer = document.querySelector('.main-container').style.display = 'none'
+    
+//     document.querySelector('#congratulations-block').classList.add('congratulations-block__style') 
+    
+//     document.querySelector('#congratulations-block__text').textContent = 'CONGRATULATIONS' 
+// }
