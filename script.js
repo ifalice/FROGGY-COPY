e5e6f364c2035f6a49476cc079c46a0a65a10cce
IFAlice <mr.spankibaster@gmail.com>
2021-12-11 01:56:43 +0200

    added func for switching lvl and func for create 'div' blocks. added list system switching lvl

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ const valueTextInput = document.querySelector('.main-container__input-text')
 
 valueTextInput.oninput = function (){
     console.log(document.querySelector('.main-container__input-text').value);
-    // document.querySelector('.main-container__input-button').setAttribute('disabled', 'disabled')
-    
+  
     document.querySelector('.flex-space__box-dinamic').style.cssText = `${document.querySelector(".main-container__input-text").value}`
     
-    
-    const inputText = document.querySelector('.main-container__input-text')
+    const inputText = document.querySelector('.main-container__input-text');
     if(inputText.value.slice(-1) !=';'){
-        if(inputText.value.split(' ').join('')+';'=='justify-content:center;'){
-            document.querySelector('.main-container__input-button').removeAttribute('disabled');      
+        if(inputText.value.split(' ').join('')+';'=== flexPositionDinamicBlock[0]){
+            document.querySelector('.main-container__input-button').removeAttribute('disabled');   
+            flexPositionDinamicBlock.shift();   
         }else if(inputText.value.slice(-1) ==';'){
-            if(inputText.value.split(' ').join('')=='justify-content:center;'){
+            if(inputText.value.split(' ').join('')===flexPositionDinamicBlock[0]){
                 document.querySelector('.main-container__input-button').removeAttribute('disabled');   
             }
         }
@@ -32,7 +31,37 @@ function lvlTwo(){
 }
 
 
-// document.querySelector('.flex-space__box-dinamic').style.cssText = `${document.querySelector(".main-container__input-text").value}`
+const lvlList = ['box-static_flex-lvl-one-jc-center','box-static_flex-lvl-two-jc-flex-end','box-static_flex-lvl-three-jc-space-between','box-static_flex-lvl-four-jc-space-around']
+const flexPositionDinamicBlock = ['justify-content:center;','justify-content:flex-end;','justify-content:space-between;','justify-content:space-around;']
+
+
+let countDoneButtonNextLvl = 1;
+function doneButtonNextLvl(){ 
+    const count_2 = ++countDoneButtonNextLvl;
+    console.log(count_2);
+    getClassName = document.querySelector('.flex-space__box-static');
+    replaceObjectsClass(getClassName,lvlList[0],lvlList[1]) 
+    lvlList.shift()
+    document.querySelector('.main-container__input-text').value = '';
+    document.querySelector('.flex-space__box-dinamic').style.cssText = `justify-content:flex-start;`;
+    if(count_2 === 3){
+        creatNewBlocks(2);    
+    }
+}
+
+
+function creatNewBlocks(numberOfElemenst){   
+    for(step=0;step<numberOfElemenst;step++){
+        let newStaticBlock = document.createElement('div');
+        newStaticBlock.classList.add('flex-space__item-static');
+        document.querySelector('.flex-space__box-static').prepend(newStaticBlock); 
+        
+        let newDinamicBlock = document.createElement('div');
+        newDinamicBlock.classList.add('flex-space__item-dinamic');
+        document.querySelector('.flex-space__box-dinamic').prepend(newDinamicBlock);     
+    }  
+         
+}
 
 
 function replaceObjectsClass(object,firstNameObjectClass, secondNameObjectClass){
@@ -40,11 +69,3 @@ function replaceObjectsClass(object,firstNameObjectClass, secondNameObjectClass)
     object.classList.add(secondNameObjectClass);
 }
 
-
-// function clickButton(){
-//     const getMainContainer = document.querySelector('.main-container').style.display = 'none'
-    
-//     document.querySelector('#congratulations-block').classList.add('congratulations-block__style') 
-    
-//     document.querySelector('#congratulations-block__text').textContent = 'CONGRATULATIONS' 
-// }
