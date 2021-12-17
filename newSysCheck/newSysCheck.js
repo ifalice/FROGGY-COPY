c8c41285ff114abe2ccb89a2a17b58683c63e0e7
ifalice <mr.spankibsater@gmail.com>
2021-12-17 21:09:39 +0200

    updated old check system and created new check textarea system

--- newSysCheck/newSysCheck.js
+++ newSysCheck/newSysCheck.js
@@ -1,4 +1,4 @@
-document.querySelector('.input').oninput = function(){
+;document.querySelector('.input').oninput = function(){
     const inputText = document.querySelector('input').value;
     // console.log(document.querySelector('input').value);
     for(step = 0;step<=1;step++){
@@ -9,7 +9,7 @@ document.querySelector('.input').oninput = function(){
     }
 }
 
-list = ['content;','center;']
+list = ['content;','center;','align;']
 
 document.querySelector('#textar').oninput = function(){
     const inputText = document.querySelector('#textar').value;
@@ -19,14 +19,58 @@ document.querySelector('#textar').oninput = function(){
     console.log(splitInpText,splitInpText.length);
     let lenInpText
     
+    let a = ''
+    
     for(stepi = 0;stepi<splitInpText.length;stepi++){
+        console.log(a);
+        
     
         for(stepj = 0; stepj<=list.length; stepj++){
-            if(splitInpText[stepi] == list[stepj]){
-                console.log('true');
-            }    
+            if(splitInpText[stepi].slice(-1) != ';'){
+                if(splitInpText[stepi] + ';' === list[stepj]){
+                    console.log('true');
+                    a+='1'
+                    if(a.length == list.length){
+                        alert('conc')
+                        break
+                    }
+                    
+                    
+
+                }
+            }else if(splitInpText[stepi].slice(-1) == ';'){
+                if(splitInpText[stepi] === list[stepj]){
+                    console.log('true');    
+                }
+                
+            }else{
+                console.log('false');
+            }
         }
         
     }
 }
+
+
+document.querySelector('#textartwo').oninput = function(){
+    const inputText = document.querySelector('#textartwo').value;
+    // console.log(document.querySelector('#textartwo').value);
+    console.log(inputText);
     
+    
+   
+    let count = ''
+    list.forEach(function(item,index) {
+        // console.log(item);
+        if(inputText.includes(item)){
+            console.log('true');
+            count+='1'
+            if(count.length === list.length){
+                alert('conc')
+            }
+        }
+
+    });
+    console.log(count);
+}
+
